075b4e92ed8bfc6cb4d60ef82312f2495b80c761
JucieLima <jucielima@gmail.com>
2025-06-04 14:14:06 -0300

    Correção de erros na inicialização do programa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.jucielima.almoxarifado {
 
     opens com.jucielima.almoxarifado to javafx.fxml;
     exports com.jucielima.almoxarifado;
+    exports com.jucielima.almoxarifado.infra.javafx.controller to javafx.fxml;
+    opens com.jucielima.almoxarifado.infra.javafx.controller to javafx.fxml;
 }
